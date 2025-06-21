35ce18b6dc02dbfd838bff6bb1797bc388da7eaf
Rodrigo Eduardo da Silva <rodrigoeduardo347@gmail.com>
2025-06-21 09:07:58 -0300

    feat: organizando rotas

--- src/routes/paths/UserRoutes.ts
+++ src/routes/paths/UserRoutes.ts
@@ -1,11 +1,30 @@
 import express from 'express';
 import { userController } from '../../container/UserContainer';
+import { validateMongoIdParams } from '../../middlewares/validateMongoIdParams';
 
 const router = express.Router();
 
-router.get('/', (req, res): Promise<any> => userController.getAll(req, res));
-router.post('/', (req, res): Promise<any> => userController.create(req, res));
-router.get('/:id', (req, res): Promise<any> => userController.getById(req, res));
-router.delete('/:id', (req, res): Promise<any> => userController.deleteById(req, res));
+router.get(
+    '/',
+    validateMongoIdParams,
+    (req, res): Promise<any> => userController.getAll(req, res)
+);
+
+router.post(
+    '/',
+    (req, res): Promise<any> => userController.create(req, res)
+);
+
+router.get(
+    '/:id',
+    validateMongoIdParams,
+    (req, res): Promise<any> => userController.getById(req, res)
+);
+
+router.delete(
+    '/:id',
+    validateMongoIdParams,
+    (req, res): Promise<any> => userController.deleteById(req, res)
+);
 
 export default router;
