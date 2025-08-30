ac087700cedf1b626ebb93fc215ede0d00999a80
Geovanna Karem <geovannakarem86@gmail.com>
2025-08-29 21:20:32 -0300

    tirando swagger do ReportRoutes + tirando uuid do json

--- src/routes/paths/ReportRoutes.ts
+++ src/routes/paths/ReportRoutes.ts
@@ -4,31 +4,6 @@ import { validateMongoIdParams } from '../../middlewares/validateMongoIdParams';
 
 const router = express.Router();
 
-/**
- * @swagger
- * /report/discipline/{id}:
- *   get:
- *     summary: Gera um relatório completo de uma disciplina.
- *     description: Retorna dados detalhados de presença e progresso da turma.
- *     tags:
- *       - Reports
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: ID da disciplina para gerar o relatório
- *     responses:
- *       200:
- *         description: Relatório da disciplina retornado com sucesso.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Report'
- *       404:
- *         description: Disciplina não encontrada.
- */
 router.get(
     '/discipline/:id',
     validateMongoIdParams,
